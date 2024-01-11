fe73f2dd5a18074b8284c6f07e8a11d65cf3ca88
Yuume Kaoo <68014278+YuumeKaoo@users.noreply.github.com>
2024-01-11 13:19:57 +0800

    Add files via upload

---
+++ Cid_Andres_TSA1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+void addItem (){
+	
+	
+	
+}
+
+void orderItem(){
+	
+	
+	
+}
+
+void checkOut(){
+	
+	
+	
+}
+
+void exit(){
+	
+	
+	
+}
+
+int main(){
+	
+	
+	
+	return 0;
+}
